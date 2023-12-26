1c021241b43e02cb119e331a3169809eb9c16092
rs <dev.lexelinfotech@gmail.com>
2023-12-26 11:20:26 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/** 
+ * Main Template File.
+ * 
+ * @package Aquila
+ * 
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    WordPress
+</body>
+</html>
